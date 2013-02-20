70930250e5ff63ca5ba19b77253a535dc8af13f9
Keisuke KITA <kei.kita2501@gmail.com>
2013-02-20 13:20:49 +0900

    fix require path

--- lib/ohajiki.rb
+++ lib/ohajiki.rb
@@ -1,6 +1,6 @@
 require "ohajiki/version"
-require File.expand_path('../config', __FILE__)
-require File.expand_path('../repo', __FILE__)
+require File.expand_path('../ohajiki/config', __FILE__)
+require File.expand_path('../ohajiki/repo', __FILE__)
 
 module Ohajiki
   class ConfigNotFound < StandardError; end
