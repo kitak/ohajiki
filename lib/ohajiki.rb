7582d5772a5f4b791e27ceaf9019c338b231595c
Keisuke KITA <kei.kita2501@gmail.com>
2013-02-20 13:42:06 +0900

    add require file

--- lib/ohajiki.rb
+++ lib/ohajiki.rb
@@ -1,5 +1,6 @@
 require "ohajiki/version"
 require File.expand_path('../ohajiki/config', __FILE__)
+require File.expand_path('../ohajiki/controller', __FILE__)
 require File.expand_path('../ohajiki/repo', __FILE__)
 
 module Ohajiki
